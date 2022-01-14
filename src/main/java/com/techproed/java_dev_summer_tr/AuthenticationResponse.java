395ab79cb7a737c23c1e015efa9e8ba2278e10ce
Suleyman Alptekin <slptkn@yahoo.com>
2022-01-14 16:07:03 -0500

    Jwt Ended

---
+++ src/main/java/com/techproed/java_dev_summer_tr/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.techproed.java_dev_summer_tr;
+
+public class AuthenticationResponse {
+	
+	private final String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+	
+}
---
+++ src/main/java/com/techproed/java_dev_summer_tr/StudentController.java
@@ -0,0 +1,41 @@
+package com.techproed.java_dev_summer_tr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@GetMapping(path = "/students")
+	public String getStudents() {
+		return "Ali Can, Veli Han, Kerem Tan";
+	}
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@PostMapping(value = "/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
+
+		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+				authenticationRequest.getPassword()));
+
+		final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
+}
